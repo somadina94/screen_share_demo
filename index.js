472b5da15507d06990042b87b45436748b9a53d0
somadina94 <somadinao@protonmail.com>
2025-05-19 00:17:31 +0100

    initial commit

--- index.js
+++ index.js
@@ -4,18 +4,13 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:5173", // your React dev server origin
-    methods: ["GET", "POST"],
-  })
-);
+app.use(cors());
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://jahbytescreen.netlify.app",
     methods: ["GET", "POST"],
   },
 });
